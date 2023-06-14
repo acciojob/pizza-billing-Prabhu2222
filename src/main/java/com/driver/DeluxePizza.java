7a4d31639ceaf05ba81918785a75da1765f7b4b0
Prabhu <prasadprabhu2222@gmail.com>
2023-06-14 20:47:26 +0530

    first commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,12 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        super.isDeluxe=true;
+        if(isVeg){
+            super.setPrice(super.getPrice()+80+70);
+        }else{
+            super.setPrice(super.getPrice()+80+120);
+        }
+
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,21 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int countExtraCheese=0;
+    private int countExtraVegToppings=0;
+    private int countExtraNonVegToppings=0;
+    private int  takeAway;
+    public boolean isDeluxe;
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.price=isVeg==true?300:400;
+        this.bill="";
     }
 
     public int getPrice(){
@@ -17,18 +28,60 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        countExtraCheese++;
+        this.price+=80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(this.isVeg){
+            countExtraVegToppings++;
+            this.price+=70;
+
+        }else{
+            countExtraNonVegToppings++;
+            this.price+=120;
+
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        takeAway++;
+        this.price+=20;
+
+
     }
 
     public String getBill(){
         // your code goes here
+       if(isVeg){
+           if(isDeluxe){
+               bill+="Base Price Of The Pizza: 450\n";
+           }else{
+               bill+="Base Price Of The Pizza: 300\n";
+           }
+       }else{
+           if(isDeluxe){
+               bill+="Base Price Of The Pizza: 600\n";
+           }else{
+               bill+="Base Price Of The Pizza: 400\n";
+           }
+       }
+       if(countExtraCheese>0){
+           bill+="Extra Cheese Added = "+(countExtraCheese*80)+"\n";
+       }
+       if(countExtraVegToppings>0){
+           bill+="Extra Toppings Added: "+(countExtraVegToppings*70)+"\n";
+       }
+       if(countExtraNonVegToppings>0){
+           bill+="Extra Toppings Added: "+(countExtraNonVegToppings*120)+"\n";
+       }
+       if(takeAway>0){
+           bill+="Paperbag Added: "+(takeAway*20)+"\n";
+       }
+       bill+="Total Price: "+this.price;
+
         return this.bill;
     }
 }
