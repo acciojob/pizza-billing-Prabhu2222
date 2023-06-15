38973882b3beeb384a2478dd3ea7bbf6350d7e86
Prabhu <prasadprabhu2222@gmail.com>
2023-06-15 09:14:35 +0530

    fourth

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -6,11 +6,11 @@ public class DeluxePizza extends Pizza {
         super(isVeg);
         // your code goes here
         super.isDeluxe=true;
-        if(isVeg){
-            super.setPrice(super.getPrice()+80+70);
-        }else{
-            super.setPrice(super.getPrice()+80+120);
-        }
+//        if(isVeg){
+//            super.setPrice(super.getPrice()+80+70);
+//        }else{
+//            super.setPrice(super.getPrice()+80+120);
+//        }
 
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -28,7 +28,7 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        if(countExtraCheese==0) {
+        if(countExtraCheese==0 && !isDeluxe) {
             this.price+=80;
         }
         countExtraCheese++;
@@ -36,13 +36,13 @@ public class Pizza {
 
     public void addExtraToppings(){
         // your code goes here
-        if(this.isVeg){
+        if(this.isVeg && !isDeluxe){
             if(countExtraVegToppings==0) {
                 this.price+=70;
             }
             countExtraVegToppings++;
 
-        }else{
+        }else if(this.isVeg==false && !isDeluxe){
             if(countExtraNonVegToppings==0) {
                 this.price+=120;
             }
@@ -63,30 +63,41 @@ public class Pizza {
     public String getBill(){
         // your code goes here
        if(isVeg){
-           if(isDeluxe){
-               bill+="Base Price Of The Pizza: 450\n";
-           }else{
                bill+="Base Price Of The Pizza: 300\n";
-           }
+
        }else{
-           if(isDeluxe){
-               bill+="Base Price Of The Pizza: 600\n";
-           }else{
                bill+="Base Price Of The Pizza: 400\n";
+       }
+
+       if(isDeluxe){
+           this.price+=80;
+           bill+="Extra Cheese Added: "+80+"\n";
+           if(isVeg){
+                this.price+=70;
+                bill+="Extra Toppings Added: "+70+"\n";
+           }else{
+               this.price+=120;
+               bill+="Extra Toppings Added: "+120+"\n";
            }
        }
-       if(countExtraCheese>0){
-           bill+="Extra Cheese Added = "+(80)+"\n";
+
+       else{
+           if(countExtraCheese>0){
+               bill+="Extra Cheese Added: "+(80)+"\n";
+           }
+           if(countExtraVegToppings>0){
+               bill+="Extra Toppings Added: "+(70)+"\n";
+           }
+           if(countExtraNonVegToppings>0){
+               bill+="Extra Toppings Added: "+(120)+"\n";
+           }
        }
-       if(countExtraVegToppings>0){
-           bill+="Extra Toppings Added: "+(70)+"\n";
-       }
-       if(countExtraNonVegToppings>0){
-           bill+="Extra Toppings Added: "+(120)+"\n";
-       }
+
        if(takeAway>0){
            bill+="Paperbag Added: "+(20)+"\n";
        }
+
+
        bill+="Total Price: "+this.price;
 
         return this.bill;
